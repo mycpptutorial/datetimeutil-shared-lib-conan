1cc8c0008d90488f22b02cf1d308eed29688c1e0
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-05-27 21:15:43 -0400

    Deleted CICD scripts and moved them into cicd repo

--- scripts/dependencies-debian.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get -y install g++
-sudo apt-get -y install cmake
-sudo pip install conan
--- scripts/dependencies-osx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-brew update
-brew install gcc
-brew install cmake
-brew install python3
-brew install pip3
-pip3 install conan
